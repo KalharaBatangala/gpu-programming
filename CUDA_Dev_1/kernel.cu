6bc58ac38dd12b967ebd18a9d155a11b1908bfaa
Kalhara Batangala <kalharabatangala@gmail.com>
2025-08-06 10:41:36 +0530

    Add project files.

---
+++ CUDA_Dev_1/kernel.cu
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+// CUDA Kernel function to add 1 to each element
+__global__ void helloFromGPU() {
+    printf("Hello World from GPU thread %d!\n", threadIdx.x);
+}
+
+int main() {
+    // Launch kernel with 5 threads
+    helloFromGPU << <1, 5 >> > ();
+
+    // Wait for GPU to finish before accessing on host
+    cudaDeviceSynchronize();
+
+    std::cout << "Hello from CPU!\n";
+    return 0;
+}
